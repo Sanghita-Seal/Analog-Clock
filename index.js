32e58ee509ee802b8e3cd1e595835b7bde80e4da
Sanghita Seal <sealsanghita2005@gmail.com>
2024-11-09 15:19:13 +0530

    A analog Clock using HTMl, CSS &JS

---
+++ index.js
@@ -0,0 +1,18 @@
+const hour = document.querySelector('.hour');
+const minute = document.querySelector('.minute');
+const sec = document.querySelector('.sec');
+
+setInterval(()=>{
+   let d= new Date();
+   let htime = d.getHours();
+   let  mtime= d.getMinutes();
+   let  stime= d.getSeconds();
+    hrotation=30*htime+ mtime/2 ;
+    mrotation=6*mtime;
+    srotation=6*stime;
+
+    hour.style.transform= `rotate(${hrotation}deg)`;
+    minute.style.transform= `rotate(${mrotation}deg)`;
+    sec.style.transform= `rotate(${srotation}deg)`;
+
+},1000);
